1be0f71d1de06fb253748ade1f95e66bb6110424
AndrewMMickel <andrew.m.mickel@gmail.com>
2021-01-13 14:12:23 -0800

    redo code to remove databases

---
+++ MusicSort/Controllers/AlbumController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using MusicOrganizer.Models;
+using System.Collections.Generic;
+
+namespace MusicOrganizer.Controllers
+{
+
+    public class AlbumsController : Controller
+    {
+        [HttpGet("/artists/{artistId}/albums/new")]
+        public ActionResult New(int artistId)
+        {
+            Artist artist = Artist.Find(artistId);
+            return View(artist);
+        }
+        [HttpGet("/artists/{artistId}/albums/{albumId}")]
+        public ActionResult Show(int artistId, int albumId)
+        {
+            Album album = Album.Find(albumId);
+            Artist artist = Artist.Find(artistId);
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            model.Add("artist", artist);
+            model.Add("album", album);
+            return View(model);
+        }
+    }
+}
